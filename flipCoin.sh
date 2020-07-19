7dca6f3fc2769f54da373f9752c6898d82f3feb6
Sree Pittala <ssapittala1@sheffield.ac.uk>
2020-07-19 11:26:25 +0100

    Problem1 and Problem2 also added in the same repo

---
+++ flipCoin.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -x
+headCount=0
+tailsCount=0
+valid=true
+
+function flipCoin() {
+	while [ valid ]
+	do
+		randomNum=$(($RANDOM%2))
+
+		if [ $randomNum -eq 0 ]
+		then
+			echo "Heads"
+			headCount=$(($headCount+1))
+		else
+			echo "Tails"
+			tailsCount=$(($tailsCount+1))
+		fi
+
+		if [ $headCount -eq 21 ]
+		then
+			result=$(($headCount-$tailsCount))
+			echo "Heads has won, with a difference of $result"
+			break;
+
+		elif [ $tailsCount -eq 21 ]
+		then
+		   result=$(($tailsCount-$headCount))
+	      echo "Tails has won, with a difference of $result"
+			break;
+
+		elif [ $headCount -eq 21 ] && [ $tailsCount -eq 21 ]
+		then
+			echo "It is a tie"
+			break;
+		else
+			echo "Result not processed"
+		fi
+	done
+	echo "Heads: $headCount"
+	echo "Tails: $tailsCount"
+}
+
+#calling the function
+flipCoin
