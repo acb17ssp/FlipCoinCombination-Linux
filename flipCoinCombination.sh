a0daff4890c512b53db41170114f6cc35d181af1
Sree Pittala <ssapittala1@sheffield.ac.uk>
2020-07-19 11:05:40 +0100

    UC: Doublet completed

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,31 +1,69 @@
-#!/bin/bash -x
+#!/bin/bash
 declare -A singlet
+
+# Heads=1, Tails=0
 
 headCount=0
 tailCount=0
 valid=true
 
-for ((i=1; i<=20; i++))
+for ((i=1; i<=10; i++))
 do
 	randomNum=$(($RANDOM%2))
 	if [ $randomNum -eq 1 ]
 	then
-		echo "H"
-		singlet[$i]="H"
 		headCount=$(($headCount+1))
 
 	elif [ $randomNum -eq 0 ]
 	then
-		echo "T"
-		singlet[$i]="T"
 		tailCount=$(($tailCount+1))
 
 	else
 		echo "Not processed"
 	fi
 done
-headsPercentage=$(($headCount*100/20))
-tailsPercentage=$(($tailCount*100/20))
+headsPercentage=$(($headCount*100/10))
+tailsPercentage=$(($tailCount*100/10))
 echo "(Singlet) Heads Percentage: $headsPercentage%"
 echo "(Singlet) Tails Percentage: $tailsPercentage%"
-echo ${singlet[@]}
+
+singlet["H"]=$headsPercentage
+singlet["T"]=$tailsPercentage
+
+
+declare -A doublet
+HH=0
+HT=0
+TH=0
+TT=0
+for ((i=1;i<=10;i++))
+do
+   randomNum1=$(( RANDOM%2 ))
+	randomNum2=$(( RANDOM%2 ))
+   combination=$randomNum1$randomNum2
+   case $combination in
+   11)
+      HH=$(( $HH+1 ))
+   ;;
+   10)
+      HT=$(( $HT+1 ))
+   ;;
+   01)
+      TH=$(( $TH+1 ))
+   ;;
+   00)
+      TT=$(( $TT+1 ))
+   ;;
+	esac
+done
+
+doublet["HH"]=$(( $HH*100/10 ))
+doublet["HT"]=$(( $HT*100/10 ))
+doublet["TH"]=$(( $TH*100/10 ))
+doublet["TT"]=$(( $TT*100/10 ))
+
+
+for key in ${!doublet[@]}
+do
+   echo "(Doublet) $key percentage: ${doublet[$key]}%"
+done
